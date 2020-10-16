06e4693f55ec19fa2ab661f143b76093c6257d45
Sarath Kaul <kaul.sarath@gmail.com>
2020-10-16 12:04:57 +0000

    Directory Changes

---
+++ src/game_api.py
@@ -0,0 +1,23 @@
+from config import _TEAM, _PASSWORD, _BASE_API
+from utils import make_request
+from requests.auth import HTTPBasicAuth
+
+
+auth = HTTPBasicAuth(_TEAM, _PASSWORD)
+
+join = _BASE_API + "/api/join"
+status = _BASE_API + "/api/gamestatus"
+guess = _BASE_API + "/api/guess"
+
+
+def join_game():
+    return make_request("POST", join, auth=auth)
+
+
+def get_game_status():
+    return make_request("GET", status)
+
+
+def make_guess(request):
+    headers = {"Content-Type": "application/json"}
+    return make_request("POST", guess, auth=auth, headers=headers, data=request)
---
+++ src/main.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, render_template
+from config import _TEAM
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":  
+    app.run(port=int(os.environ.get("PORT", 8080)))
